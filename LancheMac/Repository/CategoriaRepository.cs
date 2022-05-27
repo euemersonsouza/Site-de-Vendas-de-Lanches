f3c28ed99563d2f6ca962c01e4c39522690af644
Emerson Reis <emersonreis22664@gmail.com>
2022-05-26 22:01:35 -0300

    Adicionar arquivos de projeto.

---
+++ LancheMac/Repository/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LancheMac.Context;
+using LancheMac.Models;
+using System.Collections.Generic;
+
+namespace LancheMac.Repository
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => throw new System.NotImplementedException();
+    }
+}
---
+++ LancheMac/Repository/interface/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using LancheMac.Models;
+using System.Collections.Generic;
+
+namespace LancheMac.Repository
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
