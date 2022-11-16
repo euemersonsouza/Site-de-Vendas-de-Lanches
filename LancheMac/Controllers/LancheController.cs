04225be35fa23afd3eb1239521a7336bf25f8ea2
Emerson Reis <emersonreis22664@gmail.com>
2022-11-16 07:45:00 -0300

    Commit inicial.

--- LancheMac/Controllers/LancheController.cs
+++ LancheMac/Controllers/LancheController.cs
@@ -1,5 +1,7 @@
 ﻿using LancheMac.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace LancheMac.Controllers
 {
@@ -12,9 +14,16 @@ namespace LancheMac.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string filtro)
         {
             var lanches = _lancheRepository.Lanches;
+            ViewData["filtro"] = filtro;
+            ViewData["filtro"] = filtro;
+            if (!String.IsNullOrEmpty(filtro))
+            {
+                lanches = lanches.Where(s => s.Nome.Contains(filtro)
+                                       || s.DescricaoDetalhada.Contains(filtro));
+            }
             return View(lanches);
         }
     }
