5a16a66a2a9eecddebf7043578903ca622ed7ad7
Emerson Reis <emerson.reis@efabrika.com.br>
2022-12-06 16:44:38 -0300

    Atualização no projeto lanches com novas telas e e modelos.

---
+++ LanchesMac/Componente/CarrinhoCompraResumo.cs
@@ -0,0 +1,35 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Componente
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        public readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCiompra)
+        {
+            _carrinhoCompra = carrinhoCiompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(),
+            //    new CarrinhoCompraItem()
+            //};
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+    }
+}
